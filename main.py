a7b3023596c44acf5ebf2dfd61b18ed046eb7ae6
ilia <iliaman1993@gmail.com>
2022-03-31 12:14:22 +0300

    semi worked project

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import game_of_life
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    game_of_life.GameOfLife(25, 25)
+    return render_template('index.html')
+
+
+@app.route('/live')
+@app.route('/live.html')
+def live():
+    game = game_of_life.GameOfLife(25, 25)
+    game.life_counter += 1
+
+    return render_template('live.html', game=game)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
