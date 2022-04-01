a8b9880340be3d1c3353bdecd2b6df635c362172
ilia <iliaman1993@gmail.com>
2022-04-01 11:37:27 +0300

    added map setting

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import game_of_life
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index.html')
+@app.route('/', methods=['get', 'post'])
+@app.route('/index.html', methods=['get', 'post'])
 def index():
-    game_of_life.GameOfLife(25, 25)
-    return render_template('index.html')
+    server_message = ''
+    dl = 20
+    sh = 20
+    if request.method == 'POST':
+        if request.form.get('dl').isdigit() and request.form.get('sh').isdigit():
+            dl = int(request.form.get('dl'))
+            sh = int(request.form.get('sh'))
+            server_message = f"Красаучык. Размеры поля изменены на: { dl }x{ sh }. Наслаждайся <3"
+        elif dl == 20 and sh == 20:
+            server_message = "С тандартные размеры поля 20х20, неплохой выбор. Да? Наслаждайся <3"
+        else:
+            server_message = "Пчел введи числа"
+
+
+    game_of_life.GameOfLife(dl, sh)
+    return render_template('index.html', server_message=server_message)
 
 
 @app.route('/live')
